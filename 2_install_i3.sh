6fb5ba38c17b537eed8114068f0e740413cd54fb
Natalia Kuchta <n.kuchta@onet.pl>
2024-08-07 16:23:59 +0200

    fixed i3 compilation script
    because of release that merged i3-gaps to i3
    
    https://i3wm.org/downloads/RELEASE-NOTES-4.22.txt

--- 2_install_i3.sh
+++ 2_install_i3.sh
@@ -4,24 +4,19 @@ echo "##################################"
 echo "INSTALL I3"
 echo "##################################"
 
-rm -rf /tmp/Airblader
+rm -rf /tmp/i3
 
-git clone https://github.com/Airblader/i3.git /tmp/Airblader
+git clone https://github.com/i3/i3.git /tmp/i3
 
-cd /tmp/Airblader
+cd /tmp/i3
 
 # compile & install
-autoreconf --force --install
-rm -rf build/
-mkdir -p build && cd build/
+mkdir -p build && cd build
+meson ..
+ninja
+sudo ninja install
 
-# Disabling sanitizers is important for release versions!
-# The prefix and sysconfdir are, obviously, dependent on the distribution.
-../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
-
-make && sudo make install
-
-rm -rf /tmp/Airblader
+rm -rf /tmp/i3
 
 echo "You installed the following version"
 echo
